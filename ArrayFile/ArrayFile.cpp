f4883a9f20d59c24a8255929b4bb217ab2a737f5
vlazoryk <v.lazoryk@chnu.edu.ua>
2022-09-08 10:20:01 +0300

    fsdfd

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -159,7 +159,7 @@ void ShowMainMenu()
     cout << "    3.  Task 3  \n";
     cout << "    e.  Task 7  \n";
     cout << "    t.  Task 5  \n";
-    cout << "    5.  Task 5  \n";
+    cout << "    5.  Task 6  \n";
 
 }
 
